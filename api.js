6a07007cf15d644be3bf7fc3fe6bc1a3806f5bba
Tim Cameron Ryan <id@timryan.org>
2012-04-27 18:25:26 -0400

    POSTing methods

--- api.js
+++ api.js
@@ -6,29 +6,45 @@ function callback(call, cb) {
 
 // API object
 
-var Api = {
+var Api = window.Api = {};
 
-	getUser: function (username, cb) {
-		callback($.get('/api/users/' + username), cb)
-	},
-
-	getBind: function (id, cb) {
-		callback($.get('/api/binds/' + id), cb)
-	},
-
-	getPlace: function (id, cb) {
-		callback($.get('/api/places/' + id), cb)
-	},
-	
-	getPositions: function (cb) {
-		callback($.get('/api/positions/'), cb)
-	},
-
-	getPlaces: function (cb) {
-		callback($.get('/api/places/'), cb)
-	}
+Api.getUser = function (username, cb) {
+	callback($.get('/api/users/' + username), cb)
 };
 
-this.Api = Api;
+Api.getBind = function (id, cb) {
+	callback($.get('/api/binds/' + id), cb)
+};
+
+Api.getPlace = function (id, cb) {
+	callback($.get('/api/places/' + id), cb)
+};
+
+Api.getPositions = function (cb) {
+	callback($.get('/api/positions/'), cb)
+};
+
+Api.getPlaces = function (cb) {
+	callback($.get('/api/places/'), cb)
+};
+
+/* posting */
+
+Api.postPlace = function (floor, name, alias, cb) {
+	var args = {floor: floor, name: name, alias: alias || ''};
+	callback($.post('/api/places/', args), cb);
+};
+
+Api.postBind = function (username, place_id, x, y, signals, cb) {
+	var args = {username: username, place: place_id, x: x, y: y};
+	for (var k in signals)
+		args['signals[' + k + ']'] = signals[k];
+	callback($.post('/api/binds/', args), cb);
+};
+
+Api.postPosition = function (username, bind_id) {
+	var args = {username: username, bind: bind_id};
+	callback($.post('/api/positions/', args), cb);
+};
 
 })();
