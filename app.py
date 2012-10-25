24a4ed8ad3aea9b9337d514b7e42697ad03cf594
Julian Ceipek <julian.ceipek@gmail.com>
2012-10-25 02:19:27 -0400

    Indentation fix.

--- app.py
+++ app.py
@@ -125,23 +125,22 @@ def delete_bind(id):
 # Dot product between two signal vectors (union of routers)
 def nearest_binds(signals, limit = 10, **crit):
     signalsA = {k.lower(): v for k, v in signals.items()}
-        crit['$or'] = []
+    crit['$or'] = []
     for k, v in signalsA.items():
-                crit['$or'].append({('signals.%s' % k): {"$exists": True}})
+        crit['$or'].append({('signals.%s' % k): {"$exists": True}})
 
     matches = []
-        for bind in binds.find(crit):
+    for bind in binds.find(crit):
         signalsB = bind['signals']
 
         macs = set(signalsA.keys()).union(signalsB.keys())
+        pt1 = numpy.array([float(signalsA.get(k, 0)) for k in macs])
+        pt2 = numpy.array([float(signalsB.get(k, 0)) for k in macs])
 
-                pt1 = numpy.array([float(signalsA.get(k, 0)) for k in macs])
-                pt2 = numpy.array([float(signalsB.get(k, 0)) for k in macs])
-
-                dist = numpy.dot(
+        dist = numpy.dot(
             pt1/numpy.linalg.norm(pt1),
             pt2/numpy.linalg.norm(pt2))
-                matches.append((dist, bind))
+        matches.append((dist, bind))
 
     return [__format_bind(x[1]) for x in sorted(matches, key=itemgetter(0), reverse=True)[0:limit]]
 
